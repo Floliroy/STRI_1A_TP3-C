252da42982f66ae055fb2a8aec393190c33985b0
Floliroy <florian.leeroy@hotmail.com>
2019-12-22 17:57:47 +0100

    commented

--- mainServeur.c
+++ mainServeur.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "serveur.h"
 
+
 //extrait le fichier (et son chemin) d'une requete
 int extraitFichier(char *requete, char *tableauNomFichier, int tailleTableauNomFichier){
 	int i, cpt;
@@ -29,6 +30,7 @@ int extraitFichier(char *requete, char *tableauNomFichier, int tailleTableauNomF
 	return -1;
 }
 
+
 //retourne la taille d'un fichier en octet
 unsigned long longueur_fichier(char *nomFichier){
 	FILE *fichier = fopen(nomFichier, "r");
@@ -39,6 +41,7 @@ unsigned long longueur_fichier(char *nomFichier){
 
     return taille;
 }
+
 
 //envoie le contenu d'un fichier texte
 int envoyerContenuFichierTexte(char *nomFichier){
@@ -60,17 +63,21 @@ int envoyerContenuFichierTexte(char *nomFichier){
 	return 1;
 }
 
+
+//envoie le contenu d'un fichier binaire
 int envoyerContenuFichierBinaire(char *nomFichier){
 	FILE *fichier;
 	unsigned long taille = longueur_fichier(nomFichier);
 
+	//on ouvrele fichier en lecture binaire
 	fichier = fopen(nomFichier, "rb");
 	char *buffer = (char*)malloc(taille);
 
+	//on se place au début du fichier
 	fseek(fichier, 0, SEEK_SET);
 	fread(buffer, taille, 1, fichier);
 
-	// On envoie le fichier au client
+	// On envoie le fichier binaire au client
 	EmissionBinaire(buffer, taille);
 
 	free(buffer);
@@ -79,6 +86,8 @@ int envoyerContenuFichierBinaire(char *nomFichier){
 	return 1;
 }
 
+
+//emet le content type puis length
 int envoyerContentTypeLength(char *contentLength, char *contentType){
 	//on emet les entêtes d'une page html
 	if(Emission(contentType) != 1){
@@ -95,7 +104,8 @@ int envoyerContentTypeLength(char *contentLength, char *contentType){
 	return 1;
 }
 
-//envoie une réponse 200 OK en http
+
+//envoie une réponse 200 OK pour un fichier HTTP
 int envoyerReponse200HTML(char *nomFichier){
 	printf("200 HTML\n");
 	char contentLength[31] = "Content-length: ";
@@ -110,15 +120,19 @@ int envoyerReponse200HTML(char *nomFichier){
 		return 0;
 	}
 
+	//on envoie le content type et length
 	if(envoyerContentTypeLength(contentLength, "Content-Type: text/html; charset=UTF-8\n") != 1){
 		return 0;
 	}
 
+	//on envoie le contenu de notre fichier
 	envoyerContenuFichierTexte(nomFichier);
 	
 	return 1;
 }
 
+
+//envoie une réponse 200 OK pour un fichier JPG
 int envoyerReponse200JPG(char *nomFichier){
 	printf("200 JPG\n");
 	char contentLength[31] = "Content-length: ";
@@ -133,15 +147,19 @@ int envoyerReponse200JPG(char *nomFichier){
 		return 0;
 	}
 
+	//on envoie le content type et length
 	if(envoyerContentTypeLength(contentLength, "Content-Type: image/jpeg; charset=UTF-8\n") != 1){
 		return 0;
 	}
 
+	//on envoie le contenu de notre fichier
 	envoyerContenuFichierBinaire(nomFichier);
 	
 	return 1;
 }
 
+
+//envoie une réponse 200 OK pour un fichier ICO
 int envoyerReponse200ICO(char *nomFichier){	
 	printf("200 ICO\n");
 	char contentLength[31] = "Content-length: ";
@@ -156,10 +174,12 @@ int envoyerReponse200ICO(char *nomFichier){
 		return 0;
 	}
 
+	//on envoie le content type et length
 	if(envoyerContentTypeLength(contentLength, "Content-Type: image/x-icon; charset=UTF-8\n") != 1){
 		return 0;
 	}
 
+	//on envoie le contenu de notre fichier
 	envoyerContenuFichierBinaire(nomFichier);
 	
 	return 1;
@@ -181,14 +201,17 @@ int envoyerReponse404(char *nomFichier){
 		return 0;
 	}
 
+	//on envoie le content type et length
 	if(envoyerContentTypeLength(contentLength, "Content-Type: text/html; charset=UTF-8\n") != 1){
 		return 0;
 	}
 
+	//on envoie le contenu de notre fichier
 	envoyerContenuFichierTexte(nomFichier);
 	
 	return 1;
 }
+
 
 //envoie une réponse 404 Not FOund en http
 int envoyerReponse500(char *nomFichier){
@@ -205,15 +228,19 @@ int envoyerReponse500(char *nomFichier){
 		return 0;
 	}
 
+	//on envoie le content type et length
 	if(envoyerContentTypeLength(contentLength, "Content-Type: text/html; charset=UTF-8\n") != 1){
 		return 0;
 	}
 
+	//on envoie le contenu de notre fichier
 	envoyerContenuFichierTexte(nomFichier);
 	
 	return 1;
 }
 
+
+//est ce que la requete est une requete GET
 int isRequeteGet(char *requete){
 	if(requete[0] == 'G' && requete[1] == 'E' && requete[2] == 'T'){
 		return 1;
@@ -221,6 +248,8 @@ int isRequeteGet(char *requete){
 	return 0;
 }
 
+
+//serveur sur localhost:13214
 int main() {
 	char *message = NULL;
 	char *nomFichier = malloc(511*sizeof(char));
@@ -235,10 +264,17 @@ int main() {
 
 		if(message != NULL) {
 			printf("J'ai recu: %s\n", message);
+
+			//on verifie que la requete soit une requete GET et on recupere le nom de la ressource demandée
 			if(isRequeteGet(message) == 1 && extraitFichier(message, nomFichier, 511) != -1){
 
+				//on verifie que le fichier existe
 				if((fichier = fopen(nomFichier, "r"))){
+
+					//on récupère l'extension du fichier en cherchant le DERNIER '.' de notre chaine
 					extension = strrchr(nomFichier, '.');
+
+					//suivant l'extension on choisit si on execute la fonction JPG, ICO, ou HTML
 					if(strcmp(extension, ".jpg") == 0 || strcmp(extension, ".jpeg") == 0){
 						envoyerReponse200JPG(nomFichier);
 					}else if(strcmp(extension, ".ico") == 0){
@@ -246,12 +282,17 @@ int main() {
 					}else{
 						envoyerReponse200HTML(nomFichier);
 					}
+
+					//on ferme notre fichier qu'on a ouvert pour tester s'il existait
 					fclose(fichier);
+
 				}else{
+					//la ressource n'existe pas ou envoi un 404 Not Found
 					envoyerReponse404("404NotFound.html");
 				}
 
 			}else{
+				//ce n'est pas une requete GET ou on a eu une erreur sur le serveur
 				envoyerReponse500("500ServerError.html");
 				TerminaisonClient();
 			}
